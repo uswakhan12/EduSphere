afeb8113bf89506cd346f1b5a9ed6b9e7af1f8f5
Uswa Khan <ukhan.bscs23seecs@seecs.edu.pk>
2026-08-11 17:16:30 +0500

    Update Java module configuration

--- src/main/java/stemplatform/module-info.java
+++ src/main/java/stemplatform/module-info.java
@@ -2,7 +2,8 @@ module stemplatform.stem {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
-
+opens stemplatform.stem.gui to javafx.graphics;
+exports stemplatform.stem.gui;
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
